f6bdd1957dba8faaccc75c9e1bb02dcdeea26a0a
Maria Jimena <mja279@georgetown.edu>
2022-10-22 19:33:38 -0400

    add cards skeleton to homepage

--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { Card } from "react-bootstrap";
 import ImageSlider from "../../components/image-slider";
 import { images, jumbotron } from "../../images";
 import "./style.css";
@@ -17,13 +18,64 @@ const Home = () => {
 
       <div class="shadow-gradient-2 container-fluid">
         <div className="mt-3">
-        <ImageSlider slides={images} />
+          <ImageSlider slides={images} />
         </div>
-    
-    
+      </div>
+      <div className="my-5 container-fluid border boder-1 boder-danger">
+        <div className="row d-flex justify-content-center">
+          <Card className="mx-3" style={{ width: "18rem" }}>
+            <Card.Body>
+              <Card.Title>{entityCards[0].type}</Card.Title>
+              <Card.Text>{entityCards[0].description}</Card.Text>
+            </Card.Body>
+          </Card>
+          <Card className="mx-3" style={{ width: "18rem" }}>
+            <Card.Body>
+              <Card.Title>{entityCards[1].type}</Card.Title>
+              <Card.Text>{entityCards[1].description}</Card.Text>
+            </Card.Body>
+          </Card>
+          <Card className="mx-3" style={{ width: "18rem" }}>
+            <Card.Body>
+              <Card.Title>{entityCards[2].type}</Card.Title>
+              <Card.Text>{entityCards[2].description}</Card.Text>
+            </Card.Body>
+          </Card>
+        </div>
+        <div>
+          <h2>Servicios</h2>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Tempus
+            congue nunc volutpat risus fusce turpis lectus. Purus, gravida
+            sagittis accumsan sem odio sed. Malesuada tortor nibh nibh nulla nec
+            neque. Enim, suscipit auctor tristique pharetra, egestas risus urna
+            imperdiet felis. Suspendisse maecenas ut egestas donec fermentum.
+            Bibendum massa sit amet iaculis vitae. Odio elementum tincidunt id
+            pharetra quisque in sed risus, urna. Morbi ullamcorper vestibulum
+            maecenas molestie consequat congue.
+          </p>
+        </div>
       </div>
     </>
   );
 };
+
+let entityCards = [
+  {
+    type: "Government",
+    icon: "",
+    description: "Lorem ipsum dolor sit amet, consectetur",
+  },
+  {
+    type: "Local Entrepreneur",
+    icon: "",
+    description: "Lorem ipsum dolor sit amet, consectetur",
+  },
+  {
+    type: "Nature",
+    icon: "",
+    description: "Lorem ipsum dolor sit amet, consectetur",
+  },
+];
 
 export default Home;

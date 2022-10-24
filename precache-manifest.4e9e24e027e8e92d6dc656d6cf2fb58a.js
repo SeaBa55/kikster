e361d24e7886bd141d7939f42044740e8e01477b
sarrazol <sebastianarrazola@gmail.com>
2022-10-24 04:36:22 -0400

    Updates

---
+++ precache-manifest.4e9e24e027e8e92d6dc656d6cf2fb58a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "8209ec213faf3d207ec3",
+    "url": "/kikster/static/css/main.d576f766.chunk.css"
+  },
+  {
+    "revision": "8209ec213faf3d207ec3",
+    "url": "/kikster/static/js/main.8209ec21.chunk.js"
+  },
+  {
+    "revision": "3d7694739b4ac8b7bb36",
+    "url": "/kikster/static/js/1.3d769473.chunk.js"
+  },
+  {
+    "revision": "52b8cf9b72cf4baaeacc",
+    "url": "/kikster/static/css/2.0dabf82b.chunk.css"
+  },
+  {
+    "revision": "52b8cf9b72cf4baaeacc",
+    "url": "/kikster/static/js/2.52b8cf9b.chunk.js"
+  },
+  {
+    "revision": "0a783c492c23b9dec8c8",
+    "url": "/kikster/static/js/runtime~main.0a783c49.js"
+  },
+  {
+    "revision": "3cc2ac03374616ef1d08bc8d9467e820",
+    "url": "/kikster/index.html"
+  }
+];

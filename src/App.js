e06b2cc8187ac9ca0359309ceb41b3d1b84e61cc
sarrazol <sebastianarrazola@gmail.com>
2022-12-26 15:31:25 -0500

    added basic contacts

--- src/App.js
+++ src/App.js
@@ -27,7 +27,7 @@ function App() {
       <div className="page-content">
         <Outlet context={[isMobile, setIsMobile]}/>
       </div>
-      {/* <Footer/> */}
+      <Footer/>
     </div>
   );
 }
--- src/components/footer/index.js
+++ src/components/footer/index.js
@@ -3,13 +3,29 @@ import './style.css';
 
 const Footer = () => {
     return (
-        <footer className="footer bg-light fixed-bottom">
-            <div className="footer-container d-flex justify-content-center">
-                {/* <!-- Site version info --> */}
-                <span className="my-foot-text">© 2022 v1.0 - updated: 09/29</span>
-            </div>
+        // <footer className="footer bg-light fixed-bottom">
+        //     <div className="footer-container d-flex justify-content-center">
+        //         {/* <!-- Site version info --> */}
+        //         <span className="my-foot-text">© 2022 v1.0 - updated: 09/29</span>
+        //     </div>
+        // </footer>
+
+        <footer className="footer bg-dark pt-3">
+                <div className="d-flex row justify-content-center">
+                    <p className="fw-bold ">Contáctanos</p>
+                    <p>Cel +57 318 3593342</p>
+                    <p>hello@kikster.live</p>
+                    <p>Cra. 4 # 59-21</p>
+                    <p>Bogotá D.C. | Colombia</p>
+                </div>
+          
         </footer>
     );
 }
 
 export default Footer;
+
+// Cra. 4 # 59-21
+// Cel +57 318 3593342
+// hello@kikster.live
+// Bogotá D.C. | Colombia

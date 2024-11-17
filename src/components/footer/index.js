de543518e0ff9e42b002d8980d46a3321d5f9123
sarrazol <sebastianarrazola@gmail.com>
2024-11-17 00:02:38 -0500

    attempted to fix footer alignment but still looks bad in smaller screen sizes

--- src/components/footer/index.js
+++ src/components/footer/index.js
@@ -22,26 +22,57 @@ const Footer = () => {
         <footer className="footer bg-dark pt-3">
                 <div className="d-flex row justify-content-center">
                     <p className="fw-bold ">Contáctanos</p>
-                    <p>Si tu comunidad desarrolla proyectos de turismo rural comunitario en destinos de naturaleza y quieres articular su modelo operativo de manera funcional y sostenible, te podemos ayudar.</p>
+                        <Col xl={5}>
                     <Row className="justify-content-center">
-                        <Col>
-                            <Row className="justify-content-center"> 
-                                <Col className="justify-content-end"> <FontAwesomeIcon className='fa-fw' icon={faPhone} /></Col>
-                                <Col><p>Cel +57 318 3593342</p> </Col>
+                            <p style={{
+                                textAlign: 'start',
+                                padding: 0,
+                            }}>
+                                Si tu comunidad desarrolla proyectos de turismo rural comunitario en destinos de naturaleza y quieres articular su modelo operativo de manera funcional y sostenible, te podemos ayudar.
+                            </p>
+                    </Row>
+                    <Row className="justify-content-center">
+                        <Col xl={4}>
+                            <Row className="justify-content-start"> 
+                                <Col xl={1} className="p-0">
+                                    <FontAwesomeIcon className='fa-fw' icon={faPhone} />
+                                </Col>
+                                <Col xl={6} className="p-0">
+                                    <p>
+                                        +57 318 3593342
+                                    </p>
+                                </Col>
                             </Row>
                         </Col>
-                        <Col>
-                            <Row> 
-                                <FontAwesomeIcon className='fa-fw' icon={faPaperPlane} /><p>hello@kikster.live</p> 
+                        <Col xl={4}>
+                            <Row className="justify-content-center">
+                                <Col xl={1} className="p-0">
+                                    <FontAwesomeIcon className='fa-fw' icon={faLocationDot} />
+                                </Col>
+                                <Col xl={10} className="p-0">
+                                    <p>
+                                        Cra. 4 # 59-21 | Bogotá - Col.
+                                    </p>
+                                </Col>
                             </Row>
                         </Col>
-                        <Col>
-                            <Row> 
-                                <FontAwesomeIcon className='fa-fw' icon={faLocationDot} /><p>Cra. 4 # 59-21</p>
+                        <Col xl={4}>
+                            <Row className="justify-content-end">
+                                <Col xl={1} className="p-0">
+                                    <FontAwesomeIcon className='fa-fw' icon={faPaperPlane} />
+                                </Col>
+                                <Col xl={6} className="p-0">
+                                    <p>
+                                        hello@kikster.live
+                                    </p>
+                                </Col>
                             </Row>
                         </Col>
                     </Row>
-                    <p>Bogotá D.C. | Colombia</p>
+                    <Row>
+                        <p>kikster.live</p>
+                    </Row>
+                        </Col>
                 </div>
           
         </footer>
